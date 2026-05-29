291ff601a904afb87ebac0ed9e397c12f1fe4c76
Sheni Hamitaj <shen.i@live.com>
2026-05-29 21:34:07 +0200

    Add files via upload

---
+++ swim/sw.js
@@ -0,0 +1,38 @@
+// Service worker for Sea Swim PWA
+const CACHE = 'swim90-v1';
+const ASSETS = [
+  './',
+  './swim-plan.html',
+  './manifest.webmanifest',
+  './icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request).then((resp) => {
+        if (resp && resp.status === 200 && resp.type === 'basic') {
+          const copy = resp.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return resp;
+      }).catch(() => caches.match('./swim-plan.html'));
+    })
+  );
+});
